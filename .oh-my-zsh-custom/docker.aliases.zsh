4fe06ad2ef08f5acb8998c29073af0136da98676
Ryan Clements <rtclements@infiniteenergy.com>
2020-11-16 09:43:29 -0500

    update azurite alias

--- .oh-my-zsh-custom/docker.aliases.zsh
+++ .oh-my-zsh-custom/docker.aliases.zsh
@@ -1,4 +1,4 @@
-alias azurite='docker run -d -p 10000:10000 -p 10001:10001 -v C:/Users/RTClements/dev/azurite:/data mcr.microsoft.com/azure-storage/azurite:latest'
+alias azurite='docker run --name azurite -d -p 10000:10000 -p 10001:10001 -v $HOME/.azurite:/data mcr.microsoft.com/azure-storage/azurite:latest'
 alias dc='docker-compose'
 alias dcu='dc up'
 alias dcd='dc down'
