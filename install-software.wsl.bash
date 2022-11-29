88018e1a0cf8c7612bfd3b05bd6a6da4855ac5fa
Ryan Clements <ryanclementshax@gmail.com>
2022-11-29 11:21:00 -0500

    get puppeteer to work

--- install-software.wsl.bash
+++ install-software.wsl.bash
@@ -87,6 +87,11 @@ tfenv install latest
 # Add emoji font 🤩
 sudo apt-get install fonts-noto-color-emoji -y
 
+# To get puppeteer to work
+# https://github.com/puppeteer/puppeteer/issues/6192
+# https://askubuntu.com/questions/547151/error-while-loading-shared-libraries-libxss-so-1
+sudo apt-get install libxss1
+
 sudo touch /etc/wsl.conf
 sudo bash -c 'cat > /etc/wsl.conf <<_EOF
 [automount]
